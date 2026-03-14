31e018cfdc451da2934e51d1a2e6762f48db2b4b
Kirstel Giusti <kirmarjo@hotmail.co.uk>
2026-03-14 17:02:23 +0100

    Log Prefabs Float
    The logs now hover slightly up and down in a game-like way. I did this both with the intention of improving the overall appearance of the level, and to also use the motion to indicate to the player that those logs are intractable.

--- My CCN-Game/Assets/Scripts/Collectable.cs
+++ My CCN-Game/Assets/Scripts/Collectable.cs
@@ -4,9 +4,17 @@ public class Collectable : MonoBehaviour
 {
     public AudioClip pickupSound;       // Drag sound here in Inspector
     private AudioSource audioSource;    // The AudioSource component to play it
+    
+    // To make the prefabs flightly float
+    public float floatAmplitude = 0.25f; // how high it floats
+    public float floatFrequency = 2f;    // how fast it floats
+
+    private Vector3 startPos;
 
     private void Start()
     {
+        startPos = transform.position; // store original position
+        
         audioSource = GetComponent<AudioSource>();
         if (audioSource == null)
         {
@@ -16,6 +24,14 @@ public class Collectable : MonoBehaviour
         audioSource.playOnAwake = false; // make sure it doesn't play automatically
         audioSource.loop = false; // one shot 
         
+    }
+    
+    void Update()
+    {
+        // Move object up and down like retro collectibles
+        Vector3 pos = startPos;
+        pos.y += Mathf.Sin(Time.time * floatFrequency) * floatAmplitude;
+        transform.position = pos;
     }
 
     // ======================
